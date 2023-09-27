f5d13598bd4738810dd1fbe615aef4de79bd649b
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-27 14:26:19 -0400

    addUser mutation done in TypeDefs

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -16,7 +16,18 @@ const resolvers = {
         }
     },
     Mutation: {
+        // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
         addUser: async (parent, { username, email, password }) => {
+            try {
+                const user = await User.create({ username, email, password });
+                if (!user) {
+                    return res.status(400).json({ message: 'Something is wrong!' });
+                }
+                const token = signToken(user);
+                return { token, user };
+            } catch (err) {
+                console.error(err);
+            };
 
         },
         login: async (parent, { email, password }) => {
