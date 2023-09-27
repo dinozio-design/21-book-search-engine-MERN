5fb0f86c648f5a139f313fd628834d16b766f48d
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-27 14:37:13 -0400

    added login functionality to mutations in resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -7,6 +7,9 @@ const { User, Book } = require('../models');
 // imporing sign token function from auth
 const { signToken } = require('../utils/auth');
 
+//importing autentification checker from apollo server
+const { AuthenticationError } = require('apollo-server-express');
+
 // creating query function according to typeDefs
 const resolvers = {
     Query: {
@@ -30,8 +33,20 @@ const resolvers = {
             };
 
         },
+        // login a user, sign a token, and send it back (to client/src/components/LoginForm.js) 
         login: async (parent, { email, password }) => {
-
+            // find user by email 
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            };
+            // verify password
+            const correctPw = await profile.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            };
+            const token = signToken(user);
+            return { token, user };
         },
         saveBook: async (parent, { bookMark, user }) => {
 
