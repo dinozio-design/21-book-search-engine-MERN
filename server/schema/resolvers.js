0a0b2a123b496c38718f43ee206b90655fc8d05f
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-27 14:16:14 -0400

    framing the mutation functions according to typeDefs}

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -2,19 +2,34 @@
 // use functionality in the user-controller.js as a guide
 
 // importing user model
-const {User, Book} = require('../models');
+const { User, Book } = require('../models');
 
 // imporing sign token function from auth
-const {signToken} = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 // creating query function according to typeDefs
 const resolvers = {
     Query: {
         //get all documents from "me" collection from typeDefs
-        me: async()=>{
+        me: async () => {
             return (await User.find({})).populate('savedBooks');
         }
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+
+        },
+        login: async (parent, { email, password }) => {
+
+        },
+        saveBook: async (parent, { bookMark, user }) => {
+
+        },
+        removeBook: async (parent, { bookId, userId }) => {
+
+        }
     }
+
 };
 
 module.exports = resolvers;
