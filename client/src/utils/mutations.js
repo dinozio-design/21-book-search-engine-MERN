549e4b7578e3719a49b2b2129dfbba27a8f7e10c
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-28 13:08:39 -0400

    add user mutation is written must connect it to pages and forms in order to test its functionality

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,3 +10,27 @@ SAVE_BOOK will execute the saveBook mutation
 REMOVE_BOOK will execute the removeBook mutation
 
 */
+
+import { gql } from "@apollo/client";
+
+//LOGIN_USER will execute the loginUser mutation set up using Apollo Server
+
+export const LOGIN_USER = gql`
+
+`;
+
+//ADD_USER will execute the addUser mutation
+export const ADD_USER = gql`
+    mutation addUser(username: String!, email: String!, password: String!){
+        addUser(username: String!, email: String!, password: String!):
+        {
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,19 +3,21 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-me {
-    _id
-    username
-    email
-    bookCount
-    savedBooks {
+query Query {
+    me {
         _id
-        bookId
-        authors
-        description
-        title
-        image
-        link
+        username
+        email
+        bookCount
+        savedBooks {
+            _id
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
     }
 }
 `;
