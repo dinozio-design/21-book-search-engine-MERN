e7e78691c2631bdd3af74ab967531db891b64327
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-28 12:49:42 -0400

    get me query added.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,21 @@
 // [TODO:] This will hold the GET_ME which will execure the me query set up using Apollo Server
+
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+me {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+        _id
+        bookId
+        authors
+        description
+        title
+        image
+        link
+    }
+}
+`;
