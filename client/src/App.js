482b208e3166f6323027e71c072c445f6c9a0a04
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-28 12:32:00 -0400

    app.js updated with apollo client and ApolloProvider wrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// using apollo client instead
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 
 //[TODO:] Create an Apollo Provider to make every request work with the Apollo Server
-
+// setting up apollo client
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      {/* Wrap page elements in Router component to keep track of location state */}
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/'
+              element={<SearchBooks />}
+            />
+            <Route
+              path='/saved'
+              element={<SavedBooks />}
+            />
+            <Route
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
