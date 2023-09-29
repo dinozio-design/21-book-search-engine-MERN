3d5ee3af5a6f2837170bf4b579e59bc521662845
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-29 13:15:04 -0400

    updated gql link to make it dynamic.... next must connect authentication to link maker

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // using apollo client instead
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 
 //[TODO:] Create an Apollo Provider to make every request work with the Apollo Server
+//making gql link dynamic
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 // setting up apollo client
 const client = new ApolloClient({
-  uri: '/graphql',
+  uri: httpLink,
   cache: new InMemoryCache(),
 });
 
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -47,7 +47,7 @@ const resolvers = {
                 throw new AuthenticationError('No profile with this email found!');
             };
             // verify password
-            const correctPw = await profile.isCorrectPassword(password);
+            const correctPw = await user.isCorrectPassword(password);
             if (!correctPw) {
                 throw new AuthenticationError('Incorrect password!');
             };
